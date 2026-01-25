cc60c6135fd4ff795e93dd9249af76809b4c7022
vishnusharma17 <vishnusharma983j@gmail.com>
2026-01-25 15:41:07 +0530

    final fix

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3000,
+//     open: true
+//   },
+//   build: {
+//     outDir: 'dist',
+//     assetsDir: 'assets'
+//   }
+// })
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
 export default defineConfig({
+  base: "/Portfolio/",
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
-  }
-})
+});
 
